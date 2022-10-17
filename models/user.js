a39eabe41d7319e4a65474e6fe5f6881063b08ad
Terry Kayondo <109131914+tkterry256@users.noreply.github.com>
2022-10-18 00:47:02 +0300

    Create user model

---
+++ models/user.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const UserSchema = new mongoose.Schema({
+  uniqueID: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  fullName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  role: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  ward: {
+    type: String,
+    trim: true,
+  },
+  gender: {
+    type: String,
+    trim: true,
+  },
+  dateOfBirth: {
+    type: Date,
+    trim: true,
+  },
+  dateOfRegistration: {
+    type: Date,
+    trim: true,
+  },
+  ninNumber: {
+    type: String,
+    trim: true,
+    unique: true,
+  },
+  phoneNumber: {
+    type: String,
+    trim: true,
+    unique: true,
+  },
+  residenceDirections: {
+    type: String,
+    trim: true,
+  },
+  residenceType: {
+    type: String,
+    trim: true,
+  },
+  durationOfStay: {
+    type: Number,
+    trim: true,
+  },
+  activitiesUndertaken: {
+    type: [String],
+    trim: true,
+  },
+});
+
+UserSchema.plugin(passportLocalMongoose, { usernameField: "uniqueID" });
+
+module.exports = mongoose.model("User", UserSchema);
